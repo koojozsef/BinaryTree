e07a84518828c1e96ed0a3c10041c965a9ca2c81
Koo <uid33714@contiwan.com>
2020-07-14 11:48:16 +0200

    Node struct created

--- BinaryTreeProject/main.cpp
+++ BinaryTreeProject/main.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+bool compareTreeElementtList(const Node* rootA, const Node* rootB){
+    // return true if the two trees contain the same elements, false otherwise
+}
+
 int main()
 {
     cout << "Hello world!" << endl;
