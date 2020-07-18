2618ceab9a2332a69ec78ec66a713e4eff70f02a
Koo <uid33714@contiwan.com>
2020-07-18 11:28:25 +0200

    Comparison is implemented with one Queue Time complexity: O(n) Auxiliary space: O(n/2)

--- BinaryTreeProject/main.cpp
+++ BinaryTreeProject/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <algorithm>
 #include<stdio.h>
 #include<stdlib.h>
 
 using namespace std;
+#define CFG_COMPARE_W_ONE_SUPP_STORE 1
+#define CFG_COMPARE_W_REDUCED_SUPP_STORE 0
+
+#define RET_VAL__DEFAULT 0
+#define RET_VAL__INPUT_IS_NULL 1
+#define RET_VAL__NOT_SAME 2
+#define RET_VAL__SAME 3
 
 struct Node {
     int data;
@@ -19,6 +28,13 @@ void storeInOrder(const Node* root, vector<int> &v){
     storeInOrder(root->left, v);
     v.push_back(root->data);
     storeInOrder(root->right, v);
+}
+
+void storeInOrder(const Node* root, queue<int> &q){
+    if (root == NULL){return;}
+    storeInOrder(root->left, q);
+    q.push(root->data);
+    storeInOrder(root->right, q);
 }
 
 void compare(const Node* rootA, const Node* rootB, vector<int> &v){
@@ -56,11 +72,51 @@ void compare(const Node* rootA, const Node* rootB, vector<int> &v){
     return;
 }
 
+int compareDuringWalkThrough(const Node* root, queue<int> &q){
+    //returns instantly if no more node in specific direction
+    if (root == NULL){return RET_VAL__INPUT_IS_NULL;}
+
+    //returns if a difference is detected. No more check is needed
+    if(compareDuringWalkThrough(root->left, q) == RET_VAL__NOT_SAME){
+        return RET_VAL__NOT_SAME;
+    }
+
+    //returns if queue is empty -> it means the two trees are different
+    //returns if current (smallest) node does not match with expected value
+    //  it means the two tree not equal, since the queue is ordered
+    if (q.empty() || root->data != q.front()){
+        return RET_VAL__NOT_SAME;
+    }else{
+        printf("- %d -\n",q.front());
+        q.pop(); //removes the smallest element, not needed anymore to check
+    }
+
+    //returns if a difference is detected. No more check is needed
+    if(compareDuringWalkThrough(root->right, q) == RET_VAL__NOT_SAME){
+        return RET_VAL__NOT_SAME;
+    }
+
+    //if no above condition detected, it means trees have the same values
+    return RET_VAL__SAME;
+}
+
 bool compareTreeElementtList(const Node* rootA, const Node* rootB){
     // return true if the two trees contain the same elements, false otherwise
     vector<int> v;
+    queue<int> q;
+    bool retval = RET_VAL__DEFAULT;
+
+#if CFG_COMPARE_W_REDUCED_SUPP_STORE == 1
     compare(rootA, rootB, v);
-    return true;
+#endif // CFG_COMPARE_W_REDUCED_SUPP_STORE
+
+#if CFG_COMPARE_W_ONE_SUPP_STORE == 1
+    storeInOrder(rootA, q); // - store treeA into Queue
+    retval = (compareDuringWalkThrough(rootB, q) == RET_VAL__SAME // - compare queue with treeB during treeB recursion
+              && q.empty()); //if queue still has elements, it means trees have not the same size.
+
+#endif // CFG_COMPARE_W_ONE_SUPP_STORE
+    return retval;
 }
 
 void printAllNodes(const Node* root){
@@ -102,20 +158,35 @@ struct Node* generateStructure(vector<int> vect){
     return tempRoot;
 };
 
+void generateSequence(vector<int> &v, int from, int to){
+    for(int i=from;i<to;i++)v.push_back(i+1);
+    std::random_shuffle(v.begin(), v.end());
+}
+
 int main()
 {
     struct Node *rootA = NULL;
     struct Node *rootB = NULL;
     struct Node *rootC = NULL;
+    struct Node *rootLongA = NULL;
+    struct Node *rootLongB = NULL;
     vector<int> A = {4,2,5,1,3};
     vector<int> B = {2,1,3,4,5};
-    vector<int> C = {5,3,2,1,4};
+    vector<int> C = {5,3,2,1,4,6};
+    vector<int> longA;
+    vector<int> longB;
+
+    generateSequence(longA,0,100);
+    generateSequence(longB,0,100);
+
 
     rootA = generateStructure(A);
     rootB = generateStructure(B);
     rootC = generateStructure(C);
+    rootLongA = generateStructure(longA);
+    rootLongB = generateStructure(longB);
 
-    if(compareTreeElementtList(rootA,rootB)){
+    if(compareTreeElementtList(rootLongA,rootLongB)){
         printf("Same!");
     }else{
         printf("Not same!");
